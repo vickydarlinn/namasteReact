adef6e91a9df71eb9b17705ddf6a75e53c866a03
vickydarlinn <uttapalsangwan@gmail.com>
2022-11-28 19:39:37 +0530

    init: react app with module and bundlers

---
+++ assignment2/src/app.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const heading = React.createElement("h1", null, "hello vicky");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
