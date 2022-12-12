b83193d7df48b02660f9ccbb09da39678eec1bff
vickydarlinn <uttapalsangwan@gmail.com>
2022-12-12 10:51:23 +0530

    fix:extra code

--- assignment5/src/components/Body.js
+++ assignment5/src/components/Body.js
@@ -3,34 +3,21 @@ import DeveloperCard from "./DeveloperCard";
 function Body(props) {
   const { userData } = props;
   return (
-    <section>
-      <div className="developers">
-        {userData.map((developer) => (
-          <DeveloperCard
-            key={developer.id}
-            name={developer.developerName}
-            image={developer.developerImage}
-            location={developer.location}
-            portfolio={developer.portfolio}
-            discord={developer.discordUserId}
-            linkedin={developer.linkedinProfile}
-            company={developer.currentCompany}
-            experience={developer.Experience}
-          />
-        ))}
-      </div>
-    </section>
+    <div className="developers">
+      {userData.map((developer) => (
+        <DeveloperCard
+          key={developer.id}
+          name={developer.developerName}
+          image={developer.developerImage}
+          location={developer.location}
+          portfolio={developer.portfolio}
+          discord={developer.discordUserId}
+          linkedin={developer.linkedinProfile}
+          company={developer.currentCompany}
+          experience={developer.Experience}
+        />
+      ))}
+    </div>
   );
 }
 export default Body;
-
-// <DeveloperCard
-// name={developer.developerName}
-// image={developer.developerImage}
-// location={developer.location}
-// portfolio={developer.portfolio}
-// discord={developer.discordUserId}
-// linkedin={developer.linkedinProfile}
-// company={developer.currentCompany}
-// experience={developer.Experience}
-// />
